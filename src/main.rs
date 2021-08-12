7a8ed7c58cce652610a23887d08b2e84d54e1a64
Max Bruckner <max@maxbruckner.de>
2021-08-12 18:04:44 +0200

    Add commandline arguments via structopt

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use crate::types::Vector;
 use crate::world::World;
 use rand::rngs::SmallRng;
 use rand::SeedableRng;
+use structopt::StructOpt;
 
 pub mod agent;
 pub mod behavior;
@@ -9,15 +10,35 @@ pub mod id;
 pub mod types;
 pub mod world;
 
+#[derive(Debug, StructOpt)]
+#[structopt(name = "tag simulation", about = "Simulating a game of tag.")]
+struct Options {
+	/// How many iterations to simulate
+	#[structopt(default_value = "10000")]
+	iterations: usize,
+	/// Width of the playing field
+	#[structopt(long, default_value = "100.0")]
+	width: f64,
+	/// Height of the playing field
+	#[structopt(long, default_value = "100.0")]
+	height: f64,
+	/// Number of players
+	#[structopt(long, default_value = "100")]
+	agent_count: usize,
+}
+
 fn main() {
-	const BOUNDS: Vector = Vector::new(100.0, 100.0);
-	const AGENT_COUNT: usize = 100;
+	let options = Options::from_args();
 
 	let mut rng = SmallRng::from_entropy();
-	let mut world = World::random(BOUNDS, AGENT_COUNT, &mut rng);
+	let mut world = World::random(
+		Vector::new(options.width, options.height),
+		options.agent_count,
+		&mut rng,
+	);
 	println!("{}", world);
 
-	for _ in 0..1_000_000 {
+	for _ in 0..options.iterations {
 		world.simulate_step();
 		if world.iteration() % 1_000 == 0 {
 			println!("Iteration: {}", world.iteration());
