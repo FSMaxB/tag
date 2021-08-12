341a5342f6a037b7fca002542bf0491695b39f62
Max Bruckner <max@maxbruckner.de>
2021-08-12 13:48:55 +0200

    Implement agent movement

--- src/agent.rs
+++ src/agent.rs
@@ -1,5 +1,5 @@
 use crate::id::Id;
-use crate::types::{Absolute, Radians, Vector};
+use crate::types::{rotate_by_angle, Absolute, Radians, Vector};
 use cgmath::{Angle, InnerSpace, MetricSpace, Rad, Zero};
 use rand::Rng;
 use std::f64::consts::PI;
@@ -13,6 +13,7 @@ pub struct Agent {
 impl Agent {
 	// TIL: https://github.com/rust-lang/rust/issues/43209, also floating point division is not allowed in const fn
 	const FIELD_OF_VIEW_ANGLE: Radians = Rad((30.0 / 180.0) * PI);
+	/// How far an agent can move in one time step.
 	const VELOCITY: f64 = 10.0;
 
 	pub fn random(id: Id, bounds: Vector, random_generator: &mut impl Rng) -> Self {
@@ -48,13 +49,28 @@ impl Agent {
 	pub fn sees(&self, other: &Agent) -> bool {
 		self.viewing_angle(other).abs() <= (Self::FIELD_OF_VIEW_ANGLE / 2.0)
 	}
+
+	/// First moves in the current direction with the constant velocity, then updates the heading.
+	/// If the agent hits the wall, it stops there.
+	pub fn perform_movement(&mut self, bounds: Vector, new_heading: Radians) {
+		self.position += self.heading_vector();
+		self.position.x = self.position.x.min(bounds.x).max(0.0);
+		self.position.y = self.position.y.min(bounds.y).max(0.0);
+
+		self.heading = new_heading.normalize();
+	}
+
+	fn heading_vector(&self) -> Vector {
+		rotate_by_angle(Vector::unit_x() * Self::VELOCITY, self.heading)
+	}
 }
 
 #[cfg(test)]
 mod test {
 	use super::*;
 	use crate::id::IdSource;
-	use cgmath::{Basis2, Deg, Rotation, Rotation2};
+	use crate::types::rotate_by_angle;
+	use cgmath::Deg;
 
 	#[test]
 	fn should_calculate_distance_between_agents() {
@@ -138,8 +154,35 @@ mod test {
 		assert!(looking_agent.sees(&in_view));
 	}
 
-	fn rotate_by_angle(vector: Vector, angle: impl Into<Radians>) -> Vector {
-		let rotation: Basis2<_> = Rotation2::from_angle(angle);
-		rotation.rotate_vector(vector)
+	#[test]
+	fn should_move_around() {
+		let mut id_source = IdSource::default();
+		let bounds = Vector::new(15.0, 15.0);
+
+		let mut agent = Agent {
+			id: id_source.next().unwrap(),
+			position: Vector::zero(),
+			heading: Deg(0.0).into(),
+		};
+		// move right, turn up
+		agent.perform_movement(bounds, Deg(90.0).into());
+		assert_eq!(Radians::from(Deg(90.0)), agent.heading);
+		assert_eq!(Vector::new(10.0, 0.0), agent.position);
+
+		// move up, turn right
+		agent.perform_movement(bounds, Deg(0.0).into());
+		assert_eq!(Radians::from(Deg(0.0)), agent.heading);
+		assert_eq!(Vector::new(10.0, 10.0), agent.position);
+
+		// move right and hit wall, turn left
+		agent.perform_movement(bounds, Deg(-180.0).into());
+		assert_eq!(Radians::from(Deg(180.0)), agent.heading);
+		assert_eq!(Vector::new(15.0, 10.0), agent.position);
+
+		// move left and don't turn
+		agent.perform_movement(bounds, Deg(-180.0).into());
+		assert_eq!(Radians::from(Deg(180.0)), agent.heading); // checks the normalization as well
+		assert_eq!(5.0, agent.position.x);
+		assert_eq!(10.0, agent.position.y.round()); // NOTE: Now we start to see rounding errors
 	}
 }
--- src/types.rs
+++ src/types.rs
@@ -1,5 +1,5 @@
 use cgmath::num_traits::real::Real;
-use cgmath::{Deg, Rad, Vector2};
+use cgmath::{Basis2, Deg, Rad, Rotation, Rotation2, Vector2};
 
 pub type Vector = Vector2<f64>;
 pub type Radians = Rad<f64>;
@@ -18,4 +18,9 @@ impl<Number: Real> Absolute for Deg<Number> {
 	fn abs(&self) -> Self {
 		Deg(self.0.abs())
 	}
+}
+
+pub fn rotate_by_angle(vector: Vector, angle: impl Into<Radians>) -> Vector {
+	let rotation: Basis2<_> = Rotation2::from_angle(angle);
+	rotation.rotate_vector(vector)
 }
