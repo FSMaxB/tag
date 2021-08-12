f70b7ca1d2c508de949cafd099be7a97f25479b9
Max Bruckner <max@maxbruckner.de>
2021-08-12 14:11:54 +0200

    Update Behavior Trait

--- src/behavior.rs
+++ src/behavior.rs
@@ -1,4 +1,5 @@
 use crate::id::Id;
+use crate::types::Radians;
 use static_assertions::assert_obj_safe;
 
 #[derive(Copy, Clone)]
@@ -8,9 +9,14 @@ pub enum Role {
 }
 
 pub trait Behavior {
-	fn perform_step(&mut self, current_role: Role, current_it: Id, previous_it: Id) -> Step;
+	fn perform_step(&mut self, current_it: Id, previous_it: Id) -> Step;
 }
 
-pub struct Step;
+pub struct Step {
+	direction: Radians,
+	velocity: f64,
+}
 
+// This trait must stay object safe because the simulation engine needs to support
+// arbitrary behaviors, so dynamic dispatch is required
 assert_obj_safe!(Behavior);
