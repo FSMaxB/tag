34934c0c0f807aff2527a6a7a946e42f71361e4a
Max Bruckner <max@maxbruckner.de>
2021-08-12 10:54:32 +0200

    Remove Role enum again
    This information should probably be stored once per simulation

--- src/agent.rs
+++ src/agent.rs
@@ -1,17 +1,15 @@
 use cgmath::{Vector2, Deg};
+use crate::id::Id;
 
 pub struct Agent {
+	id: Id,
 	position: Vector2<f64>,
 	heading: Vector2<f64>,
-	role: Role,
 }
 
 impl Agent {
 	const FIELD_OF_VIEW_ANGLE: Deg<f64> = Deg(30.0);
 	const VELOCITY: f64 = 10.0;
-}
 
-pub enum Role {
-	It,
-	NotIt,
+	
 }
