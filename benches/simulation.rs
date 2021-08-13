531bbb83e2ff9a46e6cdd61378bee05ec373af72
Max Bruckner <max@maxbruckner.de>
2021-08-13 15:52:55 +0200

    Benchmark chasing behavior

--- benches/simulation.rs
+++ benches/simulation.rs
@@ -3,65 +3,102 @@ use bencher::{benchmark_group, benchmark_main};
 use rand::rngs::SmallRng;
 use rand::SeedableRng;
 
+use tag::behavior::chasing::ChasingBehavior;
 use tag::behavior::default::DefaultBehavior;
 use tag::behavior::Behavior;
 use tag::types::Vector;
 use tag::world::World;
 
 fn default_behavior_10_agents(bench: &mut Bencher) {
-	bench_with_random_world(bench, 10, || DefaultBehavior, false);
+	bench_with_random_world::<DefaultBehavior>(bench, 10, false);
 }
 
 fn default_behavior_1_000_agents(bench: &mut Bencher) {
-	bench_with_random_world(bench, 1_000, || DefaultBehavior, false);
+	bench_with_random_world::<DefaultBehavior>(bench, 1_000, false);
 }
 
 fn default_behavior_10_000_agents(bench: &mut Bencher) {
-	bench_with_random_world(bench, 10_000, || DefaultBehavior, false);
+	bench_with_random_world::<DefaultBehavior>(bench, 10_000, false);
 }
 
 fn default_behavior_100_000_agents(bench: &mut Bencher) {
-	bench_with_random_world(bench, 100_000, || DefaultBehavior, false);
+	bench_with_random_world::<DefaultBehavior>(bench, 100_000, false);
 }
 
 fn default_behavior_1_000_000_agents(bench: &mut Bencher) {
-	bench_with_random_world(bench, 1_000_000, || DefaultBehavior, false);
+	bench_with_random_world::<DefaultBehavior>(bench, 1_000_000, false);
 }
 
 fn parallel_default_behavior_10_agents(bench: &mut Bencher) {
-	bench_with_random_world(bench, 10, || DefaultBehavior, true);
+	bench_with_random_world::<DefaultBehavior>(bench, 10, true);
 }
 
 fn parallel_default_behavior_1_000_agents(bench: &mut Bencher) {
-	bench_with_random_world(bench, 1_000, || DefaultBehavior, true);
+	bench_with_random_world::<DefaultBehavior>(bench, 1_000, true);
 }
 
 fn parallel_default_behavior_10_000_agents(bench: &mut Bencher) {
-	bench_with_random_world(bench, 10_000, || DefaultBehavior, true);
+	bench_with_random_world::<DefaultBehavior>(bench, 10_000, true);
 }
 
 fn parallel_default_behavior_100_000_agents(bench: &mut Bencher) {
-	bench_with_random_world(bench, 100_000, || DefaultBehavior, true);
+	bench_with_random_world::<DefaultBehavior>(bench, 100_000, true);
 }
 
 fn parallel_default_behavior_1_000_000_agents(bench: &mut Bencher) {
-	bench_with_random_world(bench, 1_000_000, || DefaultBehavior, true);
+	bench_with_random_world::<DefaultBehavior>(bench, 1_000_000, true);
 }
 
-fn bench_with_random_world<BehaviorType>(
-	bench: &mut Bencher,
-	agent_count: usize,
-	behavior_constructor: impl Fn() -> BehaviorType,
-	parallel: bool,
-) where
-	BehaviorType: Behavior + Send + Sync + 'static,
+fn chasing_behavior_10_agents(bench: &mut Bencher) {
+	bench_with_random_world::<ChasingBehavior>(bench, 10, false);
+}
+
+fn chasing_behavior_1_000_agents(bench: &mut Bencher) {
+	bench_with_random_world::<ChasingBehavior>(bench, 1_000, false);
+}
+
+fn chasing_behavior_10_000_agents(bench: &mut Bencher) {
+	bench_with_random_world::<ChasingBehavior>(bench, 10_000, false);
+}
+
+fn chasing_behavior_100_000_agents(bench: &mut Bencher) {
+	bench_with_random_world::<ChasingBehavior>(bench, 100_000, false);
+}
+
+fn chasing_behavior_1_000_000_agents(bench: &mut Bencher) {
+	bench_with_random_world::<ChasingBehavior>(bench, 1_000_000, false);
+}
+
+fn parallel_chasing_behavior_10_agents(bench: &mut Bencher) {
+	bench_with_random_world::<ChasingBehavior>(bench, 10, true);
+}
+
+fn parallel_chasing_behavior_1_000_agents(bench: &mut Bencher) {
+	bench_with_random_world::<ChasingBehavior>(bench, 1_000, true);
+}
+
+fn parallel_chasing_behavior_10_000_agents(bench: &mut Bencher) {
+	bench_with_random_world::<ChasingBehavior>(bench, 10_000, true);
+}
+
+fn parallel_chasing_behavior_100_000_agents(bench: &mut Bencher) {
+	bench_with_random_world::<ChasingBehavior>(bench, 100_000, true);
+}
+
+fn parallel_chasing_behavior_1_000_000_agents(bench: &mut Bencher) {
+	bench_with_random_world::<ChasingBehavior>(bench, 1_000_000, true);
+}
+
+fn bench_with_random_world<BehaviorType>(bench: &mut Bencher, agent_count: usize, parallel: bool)
+where
+	BehaviorType: Behavior + Default + Send + Sync + 'static,
 {
 	let bounds = Vector::new(1000.0, 1000.0);
 	let mut random_generator = SmallRng::from_entropy();
 	let mut world = World::random(
 		bounds,
 		agent_count,
-		behavior_constructor,
+		BehaviorType::default,
 		parallel,
 		&mut random_generator,
 	);
@@ -87,4 +124,27 @@ benchmark_group!(
 	parallel_default_behavior_1_000_000_agents,
 );
 
-benchmark_main!(default_behavior, parallel_default_behavior);
+benchmark_group!(
+	chasing_behavior,
+	chasing_behavior_10_agents,
+	chasing_behavior_1_000_agents,
+	chasing_behavior_10_000_agents,
+	chasing_behavior_100_000_agents,
+	chasing_behavior_1_000_000_agents,
+);
+
+benchmark_group!(
+	parallel_chasing_behavior,
+	parallel_chasing_behavior_10_agents,
+	parallel_chasing_behavior_1_000_agents,
+	parallel_chasing_behavior_10_000_agents,
+	parallel_chasing_behavior_100_000_agents,
+	parallel_chasing_behavior_1_000_000_agents,
+);
+
+benchmark_main!(
+	default_behavior,
+	parallel_default_behavior,
+	chasing_behavior,
+	parallel_chasing_behavior,
+);
